25e1f6063eaf9ad6a168f426fd0eccae40e3999f
phillipohwotemu <ohwotemuphillip@gmail.com>
2024-03-14 04:17:42 -0700

    succesfull-jenkins-file2

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,7 +16,6 @@ import os  # Import the os module
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,9 +25,8 @@ SECRET_KEY = 'django-insecure-=m#rt-j@%w&!_)!i$^0bjlgpmtr=xc%bzev&at=bmvyw=*9ct&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# Dynamically set ALLOWED_HOSTS from environment variable
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
-
+# Explicitly set ALLOWED_HOSTS
+ALLOWED_HOSTS = ['44.212.36.244', 'localhost']
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -112,7 +107,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
